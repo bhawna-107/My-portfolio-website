a9e1f01ee3a3a1b3cd6f6780a73498a40d16ed11
bhawna-107 <57867810+bhawna-107@users.noreply.github.com>
2023-08-18 18:21:49 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,103 +1,3 @@
-// import { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { styles } from "../styles";
-// import { navLinks } from "../constants";
-// import bhawnalogo from "../assets/bhawnalogo.png";
-// import menu from "../assets/menu.svg";
-// import close from "../assets/close.svg";
-
-// const Navbar = () => {
-//   const [active, setActive] = useState();
-//   const [toggle, setToggle] = useState(false);
-//   return (
-//     <>
-//     <nav
-//       className={`${styles.paddingX}  sm:flex flex-col w-full justify-center  py-1
-//      bg-primary sticky`}
-//     >
-//       <div
-//         className="w-full flex justify-between
-//        items-center max-w-7xl mx-auto "
-//       >
-//         <Link
-//           to="/"
-//           className="flex items-center gap-2"
-//           onClick={() => {
-//             setActive("");
-//             window.scrollTo(0, 0);
-//           }}
-//         >
-//           <img
-//             src={bhawnalogo}
-//             alt="fgh"
-//             className="w-12 h-18 object-contain rounded-full"
-//           />
-//           |
-//           <p
-//             className="text-white text-[25px] 
-//           font-bold cursor-pointer flex"
-//           >
-//             Bhawna Batra
-//           </p>
-//         </Link>
-
-//         <ul className="list-none  hidden sm:flex flex flex-column gap-10 fixed">
-//           {navLinks.map((link) => (
-//             <li
-//               key={link.id}
-//               className={`${
-//                 active === link.title ? "text-white" : "text-secondary"
-//               } hover:text-white text-[18px] font-medium cursor-pointer`}
-//               onClick={() => {
-//                 setToggle(!toggle);
-//                 setActive(link.title);
-//               }}
-//             >
-//               <a href={`#${link.id}`}>{link.title}</a>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//       <div className=" sm:hidden flex flex-1 justify-end items-center">
-//         <img
-//           src={toggle ? close : menu}
-//           alt="menu"
-//           className="w-[28px] h-[28px] object-contain
-//            cursor-pointer"
-//           onClick={() => setToggle(!toggle)}
-//         />
-
-//         <div
-//           className={`${
-//             !toggle ? "hidden" : "block"
-//           } p-6 black-gradient absolute top-20 right-0 mx-4 ,min-w-[140px] z-10 rounded-xl`}
-//         >
-//           <ul className="list-none  flex  flex-1 justify-end items-start flex-col gap-10">
-//             {navLinks.map((link) => (
-//               <li
-//                 key={link.id}
-//                 className={`${
-//                   active === link.title ? "text-white" : "text-secondary"
-//                 } font-poppins text-[16px] font-medium cursor-pointer`}
-//                 onClick={() => {
-//                   setToggle(!toggle);
-//                   setActive(link.title);
-//                 }}
-//               >
-//                 <a href={`#${link.id}`}>{link.title}</a>
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       </div>
-//     </nav>
-//     </>
-//   );
-// };
-
-// export default Navbar;
-
-
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -159,7 +59,7 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <div className='sm:hidden flex flex-1 justify-end items-center cursor-pointer'>
           <img
             src={toggle ? close : menu1}
             alt='menu'
@@ -177,7 +77,7 @@ const Navbar = () => {
                 <li
                   key={nav.id}
                   className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? "text-white" : "text-black"
+                    active === nav.title ? "text-black" : "text-white"
                   }`}
                   onClick={() => {
                     setToggle(!toggle);
