07502a21e6a0fdb087229cd31e80db024711e528
bhawna-107 <57867810+bhawna-107@users.noreply.github.com>
2023-08-18 16:41:03 +0530

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -9,17 +9,17 @@ const styles = {
       "text-[black] font-medium lg:text-[20px] sm:text-[18px] xs:text-[14px] text-[12px] lg:leading-[40px]",
   
     sectionHeadText:
-      "text-[#4477CE] font-[#4477CE] md:text-[50px] sm:text-[40px] xs:text-[30px] text-[20px]",
+      "text-[#4477CE] font-[#4477CE] md:text-[50px] sm:text-[40px] xs:text-[30px] text-[30px]",
     sectionSubText:
       "sm:text-[18px] text-[14px] text-black uppercase tracking-wider",
       sectionText:
       "text-[black] font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     iconsText:
-      "text-[#4477CE] font-[#4477CE] md:text-[40px] sm:text-[30px] xs:text-[30px] text-[25px]",
+      " md:text-[40px] sm:text-[30px] xs:text-[30px] text-[25px]  hover:text-[black]",
     contactText:
     "text-[black]  md:text-[50px] sm:text-[35px] xs-text:[30px] text-[30px]",
     buttontext:
-    "bg-[#4477CE] hover:text-[black] cursor-pointer"
+    "bg-[#4477CE] hover:text-[black] cursor-pointer hover:bg-[white] hover:border-black"
   };
   
   export { styles };
